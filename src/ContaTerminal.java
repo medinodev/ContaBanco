79a45d436b19e3cb6fdfdf1d83a9051297479d4c
medinodev <medino.dev@gmail.com>
2024-03-30 11:21:02 -0300

    segundo  commit

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    int numeroConta;
+    String agenciaConta;
+    String nomeCliente;
+
+    public static void main(String[] args){
+        float saldo = 5000f;
+
+        Scanner prompt = new Scanner(System.in);
+
+        System.out.println("Digite o numero da sua agencia");
+        String agenciaConta = prompt.nextLine();
+
+        System.out.println("Digite o numero da sua conta");
+        int numeroConta = prompt.nextInt();
+        prompt.nextLine();
+
+
+        System.out.println("Digite o seu nome");
+        String nomeCliente = prompt.nextLine();
+        
+
+
+
+        System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é " + agenciaConta + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque"); 
+
     }
 }
